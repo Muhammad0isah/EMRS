1836d5cf4cd244bf70879b5e42e73a4780358494
Muhammad Ibrahim Isah <muhammadibrahimisah0@gmail.com>
2024-01-09 11:21:29 +0100

    initial commit

---
+++ app/src/main/java/com/mtech/envirotrack/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.mtech.envirotrack;
+
+import static java.lang.Thread.sleep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread splash_screen = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        splash_screen.start();
+    }
+
+}
